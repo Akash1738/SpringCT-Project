1b6941d3adef8954023025be7ec9ae7321f99603
@kki... <akashbajgire12@gmail.com>
2026-02-17 13:38:23 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,3 @@
 variable "region" {
   default = "us-east-1"
 }
-
-variable "s3_bucket" {
-  description = "S3 bucket for remote state"
-}
-
-variable "dynamodb_table" {
-  description = "DynamoDB table for state locking"
-}
